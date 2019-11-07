76dea213d710239a8461e30a621412e9d93f53a0
SawdaO <sawda.onanuga@gmail.com>
2019-11-07 15:38:46 +0000

    submitted coursework

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -5,5 +5,10 @@ from . import views
 urlpatterns = [
     path('',views.index, name='index'), #(,view function in views.py,name of)
     path('meals/', views.meals, name = 'meals'),
-    path('meals2/',  views.meals_change, name='meals-change')
+    path('food/', views.food, name = 'food'),
+    path('bev/', views.bev, name = 'bev'),
+    
+    path('meals2/',  views.meals_change, name='meals-change'),
+    path('food2/',  views.food_change, name='food-change'),
+    path('bev2/',  views.bev_change, name='bev-change')
 ]
--- restaurant/views.py
+++ restaurant/views.py
@@ -2,7 +2,7 @@
 from django.http import HttpResponse, JsonResponse, QueryDict #JsonResponse builds JSON resp object 
 from django.shortcuts import render
 from django.template import loader #what does this do...?
-from .models import Meal
+from .models import Meal, Food, Beverage
 from django.views.decorators.csrf import csrf_exempt
 # def home(request):
 #     return render(request, ''){
@@ -22,7 +22,8 @@ def index(request):
 def meals(request):
     if request.method == 'POST':
         ml = request.POST['meal-owner']
-        new_ml = Meal(meal_owner=ml)
+        feedback = request.POST['feedback']
+        new_ml = Meal(meal_owner=ml, feedback=feedback)
         new_ml.save()
         return JsonResponse({
         'id': new_ml.id,
@@ -32,6 +33,37 @@ def meals(request):
         return JsonResponse({
         'meals': list(Meal.objects.values()) #list of dictionaries, dictionaries being key values eg name: Sawda. A list of them being like a Json Payload 
         })
+def food(request):
+    if request.method == 'POST':
+        fd = request.POST['food-name']
+        desc = request.POST['food-desc']
+        cst = request.POST['food-cost']
+        new_fd = Food(food_name=fd, food_desc=desc, cost=cst)
+        new_fd.save()
+        print(new_fd)
+        return JsonResponse({
+        'id': new_fd.id,
+        'food_name' : new_fd.food_name,
+        'food_desc' : new_fd.food_desc,
+        'food_cost': new_fd.cost
+        })
+    if request.method == 'GET':
+        return JsonResponse({
+        'food': list(Food.objects.values()) #list of dictionaries, dictionaries being key values eg name: Sawda. A list of them being like a Json Payload 
+        })
+def bev(request):
+    if request.method == 'POST':
+        bv = request.POST['bev-name']
+        new_bv =Beverage(bev_name=bv)
+        new_bv.save()
+        return JsonResponse({
+        'id': new_bv.id,
+        'bev_name' : new_bev.bev_name
+        })
+    if request.method == 'GET':
+        return JsonResponse({
+        'bev': list(Beverage.objects.values()) #list of dictionaries, dictionaries being key values eg name: Sawda. A list of them being like a Json Payload 
+        }) 
 
 @csrf_exempt
 def meals_change(request):
@@ -52,14 +84,57 @@ def meals_change(request):
     if request.method == 'DELETE':
         id = int(QueryDict(request.body).get('id'))
         # print(QueryDict(request.body))
-        
+        meal_del = Meal.objects.get(id=id)
         meal_del.delete()
         response = JsonResponse({
             'result': 'success'
         })
         response.status_code = 200
         return response
-    
-      
 
+@csrf_exempt
+def food_change(request):
+    if request.method =='PUT':
+        id = int(QueryDict(request.body).get('id'))
+        new_name = QueryDict(request.body).get('name')
+        fd_ch = Food.objects.get(id = id)
+        fd_ch.food_name=new_name
+        fd_ch.save() 
+        print(fd_ch)
+        response = JsonResponse({
+            'result': 'success'
+        })
+        response.status_code = 200
+        return response
+    if request.method == 'DELETE':
+        id = int(QueryDict(request.body).get('id'))
+        food_del = Food.objects.get(id=id)
+        food_del.delete()
+        response = JsonResponse({
+            'result': 'success'
+        })
+        response.status_code = 200
+        return response
 
+@csrf_exempt
+def bev_change(request):
+    if request.method =='PUT':
+        id = int(QueryDict(request.body).get('id'))
+        new_name = QueryDict(request.body).get('name')
+        bev_ch = Beverage.objects.get(id = id)
+        bev_ch.bev_name=new_name
+        bev_ch.save() 
+        response = JsonResponse({
+            'result': 'success'
+        })
+        response.status_code = 200
+        return response
+    if request.method == 'DELETE':
+        id = int(QueryDict(request.body).get('id'))
+        bev_del = Beverage.objects.get(id=id)
+        bev_del.delete()
+        response = JsonResponse({
+            'result': 'success'
+        })
+        response.status_code = 200
+        return response
